01c3b8caf1320856d78042f10353ef937dfd6206
Gabriel Bastos <gbastosbarbosa@gmail.com>
2022-02-08 13:43:58 -0300

    .

---
+++ .history/Calculator/Program_20220207193816.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(s: Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
